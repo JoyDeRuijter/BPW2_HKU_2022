3521ab8094a57cdfdf8cab92af4271e0711734ad
Joy <joyderuijter@gmail.com>
2022-04-06 14:43:12 +0200

    Add Main Menu

--- Assets/Scripts/2DAttempt/UIManager.cs
+++ Assets/Scripts/2DAttempt/UIManager.cs
@@ -1,6 +1,9 @@
 // Written by Joy de Ruijter
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 
 public class UIManager : MonoBehaviour
 {
@@ -16,25 +19,53 @@ public class UIManager : MonoBehaviour
     #endregion
 
     #region Variables
+    [Header("Is this the Main Menu?")]
+    [SerializeField] private bool isMainMenu;
 
     [Header("References")]
+    [Space(10)]
     [SerializeField] private GameObject playerPanel;
     [SerializeField] private GameObject statusBars;
     [SerializeField] private Image HeroImage;
     [SerializeField] private Sprite[] HeroShots;
+    [SerializeField] private AudioSource clickSound;
 
     private Animator playerPanelAnim;
+    
 
     #endregion
 
     private void Start()
     {
+        if (isMainMenu)
+            return;
+
         playerPanelAnim = playerPanel.GetComponent<Animator>();
     }
 
-    private void Update()
+    public void QuitGame()
     {
-        
+        //PlayClickSound();
+        StartCoroutine(Wait(0.5f));
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+
+    public void PlayScene(string scene)
+    {
+        //PlayClickSound();
+        StartCoroutine(Wait(0.5f));
+        SceneManager.LoadScene(scene);
+    }
+
+    private IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+
+    private void PlayClickSound()
+    {
+        clickSound.Play();
     }
 
     public void OnPlayerPanelButtonClick()
